e3288ee851c5d88ed042cd2008f0da20e1b1c4ce
Md Rukon Shekh <rukon.info@gmail.com>
2024-04-04 13:11:25 +0600

    add jquery to fileuploader

--- src/components/widget/FileUploader.js
+++ src/components/widget/FileUploader.js
@@ -8,26 +8,26 @@ class FileUploader extends Component {
         this.id = this.props.id;
     }
     componentDidMount(){
-            if(typeof document == 'undefined'){
-                return;
+            if(typeof document !== 'undefined'){
+                $("#"+this.id).dmUploader({
+                    url: '/path/to/backend/upload.asp',
+                    onDragEnter:()=>{
+                        $("#"+this.id).addClass('file_drag');
+                    },
+                    onDragLeave:()=>{
+                        $("#"+this.id).removeClass('file_drag');
+                    },
+                    onInit: function(){
+                        console.log('Callback: Plugin initialized');
+                    },
+                    onUploadProgress: (id,percentage) => {
+                        console.log(id)
+                    }
+    
+                    // ... More callbacks
+                });
             }
-            $("#"+this.id).dmUploader({
-                url: '/path/to/backend/upload.asp',
-                onDragEnter:()=>{
-                    $("#"+this.id).addClass('file_drag');
-                },
-                onDragLeave:()=>{
-                    $("#"+this.id).removeClass('file_drag');
-                },
-                onInit: function(){
-                    console.log('Callback: Plugin initialized');
-                },
-                onUploadProgress: (id,percentage) => {
-                    console.log(id)
-                }
-
-                // ... More callbacks
-            });
+            
             
     }
     render() { 
