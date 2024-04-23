c538d5961cfe895bae622b2fb26fb144e2a3948e
Md Rukon Shekh <rukon.info@gmail.com>
2024-04-23 12:41:43 +0600

    Auth done

---
+++ src/components/auth/AuthWraper.js
@@ -0,0 +1,55 @@
+"use client"
+import Api from '@/inc/Api';
+import Loading from '../widget/Loading';
+import React, { Component } from 'react';
+import { redirect } from 'next/navigation'
+class AuthWraper extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            apiCalled: false,
+            user:null
+        }
+    }
+    componentDidMount(){
+        this.retriveAccount();
+    }
+    retriveAccount(){
+        let that = this, api = Api;
+        api.setUserToken();
+        api.axios().get('/me').then(res=>{
+            that.setState({
+                apiCalled:true,
+                user:res.data.type ? res.data.data.user : null
+            })
+        }).catch(error => {
+            that.setState({
+                apiCalled:true,
+                user: null
+            })
+        })
+    }
+    render() {
+        if(!this.state.apiCalled){
+            return(
+                <div className='app_init_state_wraper'>
+                    <div>
+                        <img className='logo' src="/allies-dark-logo.png" alt="Allies logo"/>
+                        <Loading/>
+                    </div>
+                    
+                </div>
+            )
+        }
+        if(!this.state.user){
+            redirect('/login')
+        }
+        return (
+            <>
+            {this.props.children}
+            </>
+        );
+    }
+}
+
+export default AuthWraper;
