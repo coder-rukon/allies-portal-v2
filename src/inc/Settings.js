8d2665b071d5aec2997238e26f8b738f4bd648c4
Md Rukon Shekh <rukon.info@gmail.com>
2024-06-20 19:36:22 +0600

    url change

--- src/inc/Settings.js
+++ src/inc/Settings.js
@@ -9,8 +9,8 @@ let Settings = {
     device_name:'web',
     userTokenKey:'allies_token',
     secondUserTokenKey:'allies_token_super',
-    apiUrl:'https://portalapi.hostrms.com/api',
-    apiAppUrl:'https://portalapi.hostrms.com',
+    apiUrl:'http://portalapi.hostrms.com/api',
+    apiAppUrl:'http://portalapi.hostrms.com',
     listingStatus:[
         {label:'Active',value:'active'},
         {label:'Inactive',value:'inactive'}
