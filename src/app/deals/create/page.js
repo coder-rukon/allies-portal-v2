2e28dd6a59a432af16caf2a899efd9d40a768013
Md Rukon Shekh <rukon.info@gmail.com>
2024-08-20 01:20:11 +0600

    Remove deal crate

--- src/app/deals/create/page.js
+++ src/app/deals/create/page.js
@@ -1,12 +1,11 @@
 "use client"
 import React, { Component } from 'react';
-import CreateDealPage from './CreateDealPage';
 
 class page extends Component {
     render() {
         return (
-            <CreateDealPage/>
-        );
+            <div></div>
+            );
     }
 }
 
